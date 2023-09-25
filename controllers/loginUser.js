4acad8d1fa5f6f98a309a2b11c4e3a7f4f8471f6
akhio <aidos.akzhol05@gmail.com>
2023-09-26 05:13:30 +0600

    auth validation

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -2,22 +2,31 @@ const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
 module.exports = async (req, res) => {
-  const { username, password } = req.body;
-
   try {
-    const user = await User.findOne({ username: username });
-    if (user) {
-      const same = await bcrypt.compare(password, user.password);
-      if (same) {
-        req.session.userID = user._id;
-        res.redirect("/");
-      } else {
-        res.redirect("/auth/login");
-      }
-    } else {
-      res.redirect("/auth/login");
-    }
+    const user = await User.findOne({ username: req.body.username });
+    await bcrypt.compare(req.body.password, user.password);
+    res.render("index");
   } catch (error) {
-    console.error(error);
+    req.flash('validationErrorsLogin', 'Incorrect username or password')
+    return res.redirect("/auth/login");
   }
 };
+
+// const { username, password } = req.body;
+
+// try {
+//   const user = await User.findOne({ username: username });
+//   if (user) {
+//     const same = await bcrypt.compare(password, user.password);
+//     if (same) {
+//       req.session.userID = user._id;
+//       res.redirect("/");
+//     } else {
+//       res.redirect("/auth/login");
+//     }
+//   } else {
+//     res.redirect("/auth/login");
+//   }
+// } catch (error) {
+//   console.error(error);
+// }
