5f639d31d12631fe87163ff6de1edf3b1dea835e
akhio <aidos.akzhol05@gmail.com>
2023-09-26 00:54:18 +0600

    validating non-empty auth

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -1,10 +1,17 @@
 const User = require("../models/User");
 
 module.exports = async (req, res) => {
-    try {
-        await User.create(req.body);
-        res.render("login");
-      } catch (err) {
-        res.redirect('/auth/register')
-      }
+  try {
+    await User.create(req.body);
+    res.render("login");
+  } catch (err) {
+    const errors = [
+      err.errors.username.properties.message,
+      err.errors.password.properties.message,
+    ];
+    req.session.validationError = errors;
+
+    //Object.keys(err.errors.properties).map(key => err.errors.properties[key].message)
+    res.redirect("/auth/register");
+  }
 };
